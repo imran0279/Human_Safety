473557ac00e937ad8cdc19762c7205f628c5881b
Al Imran <150480569+imran0279@users.noreply.github.com>
2026-05-02 00:46:23 +0600

    Create Home component with RiskMonitor

---
+++ data/page.tsx
@@ -0,0 +1,10 @@
+import RiskMonitor from "./components/RiskMonitor";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Emergency Alert</h1>
+      <RiskMonitor />
+    </main>
+  );
+}
